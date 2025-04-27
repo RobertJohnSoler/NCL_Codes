d5d4be0303bae61b2bb68478757c4a9ea3538a66
RobertJohnSoler <rsoler@hawk.iit.edu>
2025-04-27 09:06:18 -0500

    failed skipjack code

--- skipjack.py
+++ skipjack.py
@@ -1,9 +1,9 @@
 from skipjack import Skipjack
-key_bytes = bytes.fromhex("0123456789ABCDEFF0")  # your 10-byte key
+key_bytes = bytes.fromhex("6578706C6F697420696E")  # your 10-byte key
 skj = Skipjack(key_bytes)
 
 # assume ECB:
-ct = bytes.fromhex("…")    # your ciphertext
+ct = bytes.fromhex("9e59eae1fe064a3a     275596d5bdbe655c")    # your ciphertext
 pt = bytearray()
 for i in range(0, len(ct), 8):
     pt += skj.decrypt_block(ct[i:i+8])
